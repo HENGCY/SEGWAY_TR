dba4c513b7fa38f1ff37511072f0932879ea02b2
HENGCY <seanheng0920@gmail.com>
2017-06-13 16:28:24 +0200

    Create Accelerometre.h

--- STM32/Accelerometre.h
+++ STM32/Accelerometre.h
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include "stm32f30x.h"
 #include "stm32f3_discovery.h"
-#include "stm32f3_discovery_l3gd20.h"
+#include "stm32f3_discovery_lsm303dlhc.h"
 #include "math.h"
 #include "stm32f30x_gpio.h"
 #include "hw_config.h"
