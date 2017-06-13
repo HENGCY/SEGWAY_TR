aaa0a2d91ee2341b8a95fe9a85acc12b408ec57b
Dodiode <dorian.penin@gmail.com>
2017-06-13 16:22:37 +0200

    Add files via upload

--- STM32/Accelerometre.h
+++ STM32/Accelerometre.h
@@ -13,8 +13,14 @@
 #include "stm32f3_discovery.h"
 #include "stm32f3_discovery_l3gd20.h"
 #include "math.h"
+#include "stm32f30x_gpio.h"
+#include "hw_config.h"
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
+#define LSM_Acc_Sensitivity_2g     (float)     1.0f            /*!< accelerometer sensitivity with 2 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_4g     (float)     0.5f            /*!< accelerometer sensitivity with 4 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_8g     (float)     0.25f           /*!< accelerometer sensitivity with 8 g full scale [LSB/mg] */
+#define LSM_Acc_Sensitivity_16g    (float)     0.0834f         /*!< accelerometer sensitivity with 12 g full scale [LSB/mg] */
 /* Private macro -------------------------------------------------------------*/
 /* Private variables ---------------------------------------------------------*/
 
